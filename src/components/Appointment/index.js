3af21dd893739c9470a92afd82bcddb6cd6b835b
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2021-12-08 18:21:45 -0500

    Add Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+import "./styles.scss";
+
+export default function Appointment(props) {
+  const appointmentTime = () => {
+    if (props.time) {
+      return `Appointment at ${props.time}`;
+    } else {
+      return "No Appointments"
+    }
+  };
+  
+  return (
+    <article className="appointment">
+      <span>{appointmentTime()}</span>
+    </article>
+  );
+}
