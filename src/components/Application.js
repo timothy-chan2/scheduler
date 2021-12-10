9f252d7ff61bcfa3dfd91ff19f5b31335713a2a6
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2021-12-10 13:43:19 -0500

    Modify code to use the getInterview function in the Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,31 +5,47 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import getAppointmentsForDay from "../helpers/selectors";
+import getAppointmentsForDay, { getInterview } from "../helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState({ ...state, day });
 
-  const parsedAppointments = dailyAppointments.map(appointment => <Appointment
-    key={appointment.id}
-    {...appointment}
-  />);
+  const parsedAppointments = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
-      axios.get("/api/appointments")
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then(all => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+      setState(prev => ({ ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }));
     });
   }, []);
+  
+  console.log(state.interviewers);
   
   return (
     <main className="layout">
