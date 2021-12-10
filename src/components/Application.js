021a8f2e47092a4085576b17c4f6e60dcced323c
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2021-12-09 19:10:13 -0500

    Remove days array and instead have days as a state and use axios to retreive data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -64,10 +47,19 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
   const parsedAppointments = appointments.map(appointment => <Appointment
     key={appointment.id}
     {...appointment}
   />);
+
+  useEffect(() => {
+    const daysURL = "/api/days";
+    axios.get(daysURL).then(response => {
+      setDays([...response.data]);
+    });
+  }, []);
   
   return (
     <main className="layout">
