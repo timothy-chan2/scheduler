101da698bf8279a4455974c01eabcdaaeab9c744
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2021-12-09 23:13:25 -0500

    Combine the useState into one and refactor accordingly

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,8 +46,13 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   const parsedAppointments = appointments.map(appointment => <Appointment
     key={appointment.id}
@@ -72,8 +77,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
