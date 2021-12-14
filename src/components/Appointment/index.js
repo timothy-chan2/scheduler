6ef817127d71949e4bfbb4202d7bd8382a22d99f
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2021-12-13 19:25:01 -0500

    Add to show SAVING mode before saving is completed

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,12 +6,14 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "../../hooks/useVisualMode";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
   
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -22,11 +24,14 @@ export default function Appointment(props) {
       student: name,
       interviewer: interviewer
     };
-    props.bookInterview(props.id, interview);
-    transition(SHOW);
+    
+    transition(SAVING);
+    props.bookInterview(props.id, interview)
+      .then(() => {
+        transition(SHOW);
+      });
   }  
 
-  console.log('This is props.interview:', props);
   return (
     <article className="appointment">
       <Header time={props.time}/>
@@ -38,6 +43,7 @@ export default function Appointment(props) {
         />
       )}
       {mode === CREATE && <Form interviewers={props.interviewers} onCancel={back} onSave={save} />}
+      {mode === SAVING && <Status message="Saving" />}
     </article>
   );
 }
