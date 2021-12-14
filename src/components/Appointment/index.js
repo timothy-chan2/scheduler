582146e603fac30c2242a44b640783fcfa6f001f
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2021-12-13 20:29:29 -0500

    Add deleteAppointment function and DELETING mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -14,6 +14,7 @@ export default function Appointment(props) {
   const SHOW = "SHOW";
   const CREATE = "CREATE";
   const SAVING = "SAVING";
+  const DELETING = "DELETING";
   
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -24,13 +25,23 @@ export default function Appointment(props) {
       student: name,
       interviewer: interviewer
     };
-    
+
     transition(SAVING);
     props.bookInterview(props.id, interview)
       .then(() => {
         transition(SHOW);
       });
-  }  
+  };
+
+  function deleteAppointment() {
+    const interview = null;
+
+    transition(DELETING);
+    props.cancelInterview(props.id, interview)
+      .then(() => {
+        transition(EMPTY);
+      });
+  };
 
   return (
     <article className="appointment">
@@ -40,10 +51,12 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={deleteAppointment}
         />
       )}
       {mode === CREATE && <Form interviewers={props.interviewers} onCancel={back} onSave={save} />}
       {mode === SAVING && <Status message="Saving" />}
+      {mode === DELETING && <Status message="Deleting" />}
     </article>
   );
 }
