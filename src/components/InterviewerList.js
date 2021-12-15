780a78039a39500412aa1a950b86f41300705eda
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2021-12-14 23:34:36 -0500

    Add validation check that props interviewers is an Array and that it is required

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import InterviewerListItem from "./InterviewerListItem";
 
@@ -21,4 +22,8 @@ export default function InterviewerList(props) {
       </ul>
     </section>
   );
+};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
 };
